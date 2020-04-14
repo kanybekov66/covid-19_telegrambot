72b4c796772eefdf275781e8aa39e33e430d4f1c
Azatbek <kanybekov66@gmak.com>
2020-04-14 17:53:10 +0600

    Mukhtarcheg <3

---
+++ main.py
@@ -0,0 +1,53 @@
+import telebot
+from telebot import types
+import COVID19Py
+
+covid19 = COVID19Py.COVID19()
+bot = telebot.TeleBot('981563400:AAHgKqCZIF0rBRcNmmS5_upaFDMJ0pYGPfs')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn1 = types.KeyboardButton('Во всём мире')
+    btn2 = types.KeyboardButton('Кыргызстан')
+    markup.add(btn1, btn2)
+
+    send_message = f"Привет {message.from_user.first_name}!"
+    bot.send_message(message.chat.id, send_message, parse_mode='html', reply_markup=markup)
+
+# Функция, что сработает при отправке какого-либо текста боту
+# Здесь мы создаем отслеживания данных и вывод статистики по определенной стране
+location = covid19.getLocationByCountryCode("KG")
+info = location[0]
+info2 = info['latest']
+info3 = info2['confirmed']
+
+@bot.message_handler(content_types=['text'])
+def mess(message):
+    if message.text == 'Кыргызстан':
+
+        bot.send_message(message.from_user.id, info3)
+    else:
+        bot.send_message(message.from_user.id, 'Press the fucking button')
+
+    # get_message_bot = message.text.strip().lower()
+    # if get_message_bot == "кыргызстан":
+    # 	location = covid19.getLocationByCountryCode("KG")
+    # else:
+    # 	location = covid19.getLatest()
+    # final_message = f"Данные по всему миру: Заболевших: {location['confirmed']:,} Сметрей: {location['deaths']:,}"
+    #
+    # if final_message == "":
+    # 	date = location[0]['last_updated'].split("T")
+    # 	time = date[1].split(".")
+    # 	final_message = f"Данные по стране: Население: {location[0]['country_population']:,}" \
+    # 			f"Последнее обновление: {date[0]} {time[0]}<br>Последние данные:" \
+    # 			f"Заболевших: {location[0]['latest']['confirmed']:,} Сметрей: " \
+    # 			f"{location[0]['latest']['deaths']:,}"
+    #
+    # bot.send_message(message.chat.id, final_message, parse_mode='html')
+
+# Это нужно чтобы бот работал всё время
+bot.polling(none_stop=True)
+
+#print(location)
---
+++ test.py
@@ -0,0 +1,6 @@
+import COVID19Py
+covid19 = COVID19Py.COVID19()
+x = covid19.getLatest()
+y = covid19.getLatestChanges()
+print(x)
+print(y)
